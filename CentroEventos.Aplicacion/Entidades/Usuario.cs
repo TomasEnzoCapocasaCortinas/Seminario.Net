4e923e4e2f2ff7cafc0dffdca34965f6477871e8
GinevraDCV <jazminapariciob@gmail.com>
2025-06-19 11:57:35 -0300

    ..

--- CentroEventos.Aplicacion/Entidades/Usuario.cs
+++ CentroEventos.Aplicacion/Entidades/Usuario.cs
@@ -12,4 +12,6 @@ public class Usuario
     public string Contrasenia { get; set; } = "";
     public List<Permiso> Permisos { get; set; } = new List<Permiso>();
     public DateTime FechaCreacion { get; set; } = DateTime.Now;
+
+    //protected Usuario() {} //Constructor para EF Code
 }
