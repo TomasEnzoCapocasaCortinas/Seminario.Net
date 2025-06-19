bf169da9bc39de442ae7516a15689d6dba08aea7
GinevraDCV <jazminapariciob@gmail.com>
2025-06-19 13:50:03 -0300

    Update Usuario.cs

--- CentroEventos.Aplicacion/Entidades/Usuario.cs
+++ CentroEventos.Aplicacion/Entidades/Usuario.cs
@@ -1,6 +1,7 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 using CentroEventos.Aplicacion.Enumerativos;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CentroEventos.Aplicacion.Entidades;
 
@@ -8,23 +9,22 @@ public class Usuario
 {
     public int Id { get; set; }
     //[Required(ErrorMessage = "El nombre es obligatorio")]
-    public string Nombre { get; set; }
+    public string Nombre { get; set; } = string.Empty;
     //[Required(ErrorMessage = "El apellido es obligatorio")]
-    public string Apellido { get; set; }
+    public string Apellido { get; set; }= string.Empty;
     //[Required(ErrorMessage = "El Email es obligatorio")]
-    public string Email { get; set; }
+    public string Email { get; set; }= string.Empty;
     //[Required(ErrorMessage = "La contraseña es obligatoria")]
-    public string Contrasenia { get; set; } 
-    public List<Permiso> Permisos { get; set; }
-    public DateTime FechaCreacion { get; set; }
-    public Usuario()
+    public string Contrasenia { get; set; } = string.Empty;
+    public string PermisosL { get; set; } = "";
+    [NotMapped]
+    public List<Permiso> Permisos
     {
-        this.Id = 0;
-        this.Nombre = string.Empty;
-        this.Apellido = string.Empty;
-        this.Email = string.Empty;
-        this.Contrasenia = string.Empty;
-        this.Permisos = new List<Permiso>();
-        this.FechaCreacion = DateTime.Now;
+        get => PermisosL
+                    .Split(',', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(p => Enum.Parse<Permiso>(p)).ToList();
+
+        set => PermisosL = string.Join(",", value.Select(p => p.ToString()));
     }
+    public DateTime FechaCreacion { get; set; }
 }
