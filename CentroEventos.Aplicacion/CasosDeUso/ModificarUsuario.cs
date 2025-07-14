68bfa76b3b971ca154e8891f3874aad4408415c4
GinevraDCV <jazminapariciob@gmail.com>
2025-07-14 18:09:33 -0300

    modificar usuario

--- CentroEventos.Aplicacion/CasosDeUso/ModificarUsuario.cs
+++ CentroEventos.Aplicacion/CasosDeUso/ModificarUsuario.cs
@@ -9,21 +9,16 @@ namespace CentroEventos.Aplicacion.CasosDeUso;
 public class ModificarUsuario
 {
     private readonly IRepositorioUsuario RepositorioU;
-    private readonly IServicioAutorizacion Autorizacion;
     private readonly ValidadorUsuario Validador;
 
-    public ModificarUsuario(IRepositorioUsuario repositorio, IServicioAutorizacion autorizador, ValidadorUsuario validador)
+    public ModificarUsuario(IRepositorioUsuario repositorio, ValidadorUsuario validador)
     {
         RepositorioU = repositorio;
-        Autorizacion = autorizador;
         Validador = validador;
     }
 
-    public void Ejecutar(Usuario usuarioActualizado, int idUsuario)
+    public void Ejecutar(Usuario usuarioActualizado)
     {
-        if (!Autorizacion.PoseeElPermiso(idUsuario, Permiso.PersonaModificacion))
-            throw new FalloAutorizacionException("No tiene permiso para modificar usuarios");
-
         var usuario = RepositorioU.ObtenerPorId(usuarioActualizado.Id)
             ?? throw new EntidadNotFoundException("El usuario no existe");
 
